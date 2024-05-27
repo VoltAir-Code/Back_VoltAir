caef1026d33b5ced74b3b971a6410d3863340292
Artur Fiorentino <49957976885@info.local>
2024-05-27 10:55:55 -0300

    configurção JWt Bearer finalizada

---
+++ Api/VoltAir/Repositories/UsuarioRepository.cs
@@ -0,0 +1,29 @@
+﻿using VoltAir.Contexts;
+using VoltAir.Domains;
+using VoltAir.Interfaces;
+
+namespace VoltAir.Repositories
+{
+    public class UsuarioRepository : IUsuario
+    {
+        VoltaireContext ctx = new VoltaireContext();
+        public Usuario GetByEmailPassword(string email, string password)
+        {
+            try
+            {
+                var userSearch = ctx.Usuarios.FirstOrDefault(x => x.Email == email && x.Senha == password);
+
+                return userSearch;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+   
+        }
+
+
+
+    }
+}
