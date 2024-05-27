bc33fb3a65b5fe6ac56803ac39508ab674af02a9
Marcelo <marcelocardoso020304@gmail.com>
2024-05-27 10:16:51 -0300

    Feito o scaffold

---
+++ VoltAir/VoltAir/Domains/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoltAir.Domains;
+
+public partial class Marca
+{
+    public Guid IdMarca { get; set; }
+
+    public string? NomeMarca { get; set; }
+
+    public virtual ICollection<Carro> Carros { get; set; } = new List<Carro>();
+}
---
+++ VoltAir/VoltAir/Domains/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VoltAir.Domains;
+
+public partial class Usuario
+{
+    public Guid IdUsuario { get; set; }
+
+    public Guid? IdCarro { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Senha { get; set; } = null!;
+
+    public string? Foto { get; set; }
+
+    public virtual Carro? IdCarroNavigation { get; set; }
+}
