1831212a73cfd50151b29c6893f889ea822c052c
Artur Fiorentino <49957976885@info.local>
2024-05-28 08:52:12 -0300

    api adicionada

---
+++ Api/VoltAir/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VoltAir.Domains;
+using VoltAir.Interfaces;
+using VoltAir.Repositories;
+
+namespace VoltAir.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuario usuarioRepository {  get; set; }
+
+        public UsuarioController()
+        {
+            usuarioRepository = new UsuarioRepository();
+        }
+
+
+    }
+}
