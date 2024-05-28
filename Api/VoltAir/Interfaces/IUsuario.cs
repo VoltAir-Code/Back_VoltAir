ff3e26fedc3d7d627aeecb945ed8d11e10d69540
Marcelo <marcelocardoso020304@gmail.com>
2024-05-28 08:35:42 -0300

    Resolvendo git

--- Api/VoltAir/Interfaces/IUsuario.cs
+++
@@ -1,9 +0,0 @@
-﻿using VoltAir.Domains;
-
-namespace VoltAir.Interfaces
-{
-    public interface IUsuario
-    {
-        Usuario GetByEmailPassword(string email, string password);
-    }
-}
--- VoltAir/VoltAir/Interfaces/IUsuarioRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using VoltAir.Domains;
-
-namespace VoltAir.Interfaces
-{
-    public interface IUsuarioRepository
-    {
-        public void UserRegister(Usuario usuario);
-    }
-}
